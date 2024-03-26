852fb0a18941c313c404aae213226ce7c7fb96b4
Eddiezon-lemayan <Eddymacharia450@gmail.com>
2024-03-26 11:20:00 +0300

    update1

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+    return cats
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats;
+}
+function appendCat(){
+    let appendedCats = [...cats, "Broom"]
+    return appendedCats
+}
+function prependCat(){
+    let prependCats = ["Arnold",...cats]
+    return prependCats
+}
+function removeLastCat(){
+    let removes =  cats.slice(0, cats.length - 1)
+    return removes
+}
+function removeFirstCat(){
+    let removes =  cats.slice(1)
+    return removes
+}
